a3b86eb33c8b9c291ccb68fb338db27c36713341
Coronne <coronne@Coronnes-MacBook-Pro.local>
2024-04-05 11:52:26 +0800

    same as previous

--- clients.py
+++ clients.py
@@ -2,6 +2,7 @@ from typing import List
 import requests
 import asyncio
 from servers import MockServer
+from time import time
 
 async def req_delay(server: MockServer):
     await asyncio.sleep(3)
@@ -9,18 +10,23 @@ async def req_delay(server: MockServer):
     print(bytes.decode(res.content, res.encoding))
 
 async def req(server: MockServer):
+    t = time()
     res = requests.get(f"http://{server.server_address}/get")
+    t = time() - t
+    server.avg_response_time = server.avg_response_time * server.total_requests
+    server.total_requests += 1
+    server.avg_response_time = (server.avg_response_time + t*1000)/server.total_requests
     print(bytes.decode(res.content, res.encoding))
 
-async def request_test():
-    for port in range(5000, 5010):
-        # requests will take 3 seconds longer if port is odd to test asynchronous execution
-        res = asyncio.to_thread(req if port % 2 == 0 else req_delay, port)
-        asyncio.create_task(res)
+# async def request_test():
+#     for port in range(5000, 5010):
+#         # requests will take 3 seconds longer if port is odd to test asynchronous execution
+#         res = asyncio.to_thread(req if port % 2 == 0 else req_delay, port)
+#         asyncio.create_task(res)
 
-async def request_test(balancer):
-    for _ in range(10):
-        server = balancer.get_next_server()
-        serverInstance = server.get_instance()
-        print("server: ", server.get_instance())
-        asyncio.create_task(req(serverInstance))
+# async def request_test(balancer):
+#     for _ in range(10):
+#         server = balancer.get_next_server()
+#         serverInstance = server.get_instance()
+#         print("server: ", server.get_instance())
+#         asyncio.create_task(req(serverInstance))
